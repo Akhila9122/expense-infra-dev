500f5edfae4a2cedd0dc1388273651f78c53bcff
Akhila9122 <akhilasaikam990@gmail.com>
2025-05-05 18:29:51 +0530

    sg

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -245,32 +245,4 @@ resource "aws_security_group_rule" "bastion_public" {
 #   security_group_id = module.frontend.sg_id
 # }
 
-resource "aws_security_group_rule" "vpn_443" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = module.vpn_sg.sg_id
-}
-
-
-resource "aws_security_group_rule" "vpn_943" {
-  type              = "ingress"
-  from_port         = 943
-  to_port           = 943
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = module.vpn_sg.sg_id
-}
-
-resource "aws_security_group_rule" "vpn_1194" {
-  type              = "ingress"
-  from_port         = 1194
-  to_port           = 1194
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = module.vpn_sg.sg_id
-}
-
 
